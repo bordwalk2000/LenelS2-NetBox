47f8dab029451077542dc1f7aafcf1f0837c255b
Bradley Herbst <bordwalk2000@gmail.com>
2022-07-05 14:28:26 -0500

    Updated Get-S2PersonAccesLevel Command
    Updated the Get-S2PersonAccessLevel command to make it more dynamic in
    what types of import data it could take when specifying a person.

--- Source/Public/access-levels/Get-S2PersonAccessLevel.ps1
+++ Source/Public/access-levels/Get-S2PersonAccessLevel.ps1
@@ -1,6 +1,27 @@
 Function Get-S2PersonAccessLevel {
     param(
-        [parameter(Mandatory=$true)][String] $PersonID
+        [parameter(Mandatory=$true, ParameterSetName = 'ID')][String] $S2_PersonID,
+        [parameter(ParameterSetName = 'Search')][String] $FirstName,
+        [parameter(ParameterSetName = 'Search')][String] $LastName,
+        [parameter(ParameterSetName = 'Search')][int] $PersonID
     )
-    $(Get-S2Person $PersonID).ACCESSLEVELS.ACCESSLEVEL
+
+    if ([string]::IsNullOrEmpty($S2_PersonID)) {
+        $Params = @{
+            "FirstName" = $FirstName
+            "LastName" = $LastName
+            "UDF3" = $PersonID
+        }
+        foreach( $Key in @($Params.Keys) ){
+            if( -not $Params[$Key] ){
+                $Params.Remove($Key)
+                }
+            }
+
+        $S2_PersonID = Search-S2Person @Params | Select-Object -ExpandProperty PersonID
+    }
+
+    Return Get-S2Person $S2_PersonID
+    | Select-Object -ExpandProperty ACCESSLEVELS
+    | Select-Object -ExpandProperty ACCESSLEVEL
 }
