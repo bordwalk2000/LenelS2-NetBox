8c516355d51c2d88a4f9e6c09fe802f4874f6a62
Bradley Herbst <bordwalk2000@gmail.com>
2022-07-05 14:24:21 -0500

    Return more Information about API Error
    Added a switch to return more information about the API error message.

--- Source/Public/Connect-S2Service.ps1
+++ Source/Public/Connect-S2Service.ps1
@@ -50,7 +50,33 @@ Function Connect-S2Service {
 
     if($ReturnXML.NETBOX.RESPONSE.APIERROR)
     {
-        throw "Unable to connect to NETBOX-API.  Error code: $($ReturnXML.NETBOX.RESPONSE.APIERROR)"
+        switch ($ReturnXML.NETBOX.RESPONSE.APIERROR) {
+            "1" {
+                    $apiError = "API_INIT_FAIL"
+                    $apiErrorDescription = "Database error (database not running, etc.)"
+                }
+            "2" {
+                    $apiError = "API_DISABLED"
+                    $apiErrorDescription = "The API processor is not activated for this eMerge"
+                }
+            "3" {
+                    $apiError = "API_NOCOMMAND"
+                    $apiErrorDescription = "No APIcommand parameter was passed to the API processor"
+                }
+            "4" {
+                    $apiError = "API_PARSE_ERROR"
+                    $apiErrorDescription = "The APIcommand data could not be parsed by the XML parser"
+                }
+            "5" {
+                    $apiError = "API_AUTH_FAILURE"
+                    $apiErrorDescription = "API authorization failure (if authorization enabled in the user interface)"
+                }
+            "6" {
+                    $apiError = "API_UNKNOWN_COMMAND"
+                    $apiErrorDescription = "The API processor did not recognize the command passed in the APIcommand parameter."
+                }
+        }
+        throw "Unable to connect to NETBOX-API.  Error code: $($ReturnXML.NETBOX.RESPONSE.APIERROR),  $($apiError): $($apiErrorDescription)"
     }
     else
     {
